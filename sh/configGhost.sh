bbec399b6e42b95c11c49e0a3f8ab13903d08137
pis0 <msdraco@gmail.com>
2015-05-13 15:16:38 -0300

    dev

---
+++ sh/configGhost.sh
@@ -0,0 +1,15 @@
+
+
+# config 
+export ASSUKAR_PATH="C:\workspace\Assukar\dev\client"
+export PRAIA_PATH="C:\workspace\Praia\dev\client"
+export MAGIC_PATH="C:\workspace\Magic\dev\client"
+export STARLING_PATH="C:\workspace\_starling\release"
+
+export LIB_PATH="$ASSUKAR_PATH\libs"
+
+export OUT_PATH="C:\workspace\#PIPA\bin"
+
+export FP_DEBUG="C:\workspace\_fpDebug\flashplayer_17_sa_debug.exe"
+
+export TARGET_PLAYER=17
