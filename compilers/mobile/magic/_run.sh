f80cc0323d98b2788f8131f2754dd5aaf47b5e56
Andre Ramos <forceramos@hotmail.com>
2015-05-21 19:09:22 -0300

    mob magic

--- compilers/mobile/magic/_run.sh
+++ compilers/mobile/magic/_run.sh
@@ -7,7 +7,7 @@ echo "run app ..."
 runArgs=(
 	"-launchApp"
 	"-platform $PLATAFORM"
-	"-appid com.pipastudios.release.praiabingo"
+	"-appid com.pipastudios.release.casinomagic"
 )
 $SDK/bin/adt.bat ${runArgs[@]}
 
