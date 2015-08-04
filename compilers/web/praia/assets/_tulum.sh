445042021ebc6f4bb54e75d80d5394ffdc70b05b
cmurakawa <c.mrakawa@gmail.com>
2015-08-04 16:46:22 -0300

    dev

---
+++ compilers/web/praia/assets/_tulum.sh
@@ -0,0 +1,42 @@
+
+# TULUM
+
+inOut=(	
+	# TulumAsyncSoundsSwf
+	"$PRAIA_IN_PATH/tulum/view/src/com/assukar/praia/tulum/assets/imports/TulumAsyncSoundsSwf.as"
+	"$PRAIA_OUT_PATH/TulumAsyncSoundsSwf.swf"
+	# TulumAsyncSpritesSwf
+	"$PRAIA_IN_PATH/tulum/view/src/com/assukar/praia/tulum/assets/imports/TulumAsyncSpritesSwf.as"
+	"$PRAIA_OUT_PATH/TulumAsyncSpritesSwf.swf"
+	# TulumSyncSoundsSwf
+	"$PRAIA_IN_PATH/tulum/view/src/com/assukar/praia/tulum/assets/imports/TulumSyncSoundsSwf.as"
+	"$PRAIA_OUT_PATH/TulumSyncSoundsSwf.swf"
+	# TulumSyncSpritesSwf
+	"$PRAIA_IN_PATH/tulum/view/src/com/assukar/praia/tulum/assets/imports/TulumSyncSpritesSwf.as"
+	"$PRAIA_OUT_PATH/TulumSyncSpritesSwf.swf"
+)
+
+
+sourcePaths=(
+	"-source-path+=$ASSUKAR_IN_PATH/airong/src"
+	"-source-path+=$ASSUKAR_IN_PATH/engine/src"
+	"-source-path+=$ASSUKAR_IN_PATH/domain/src"
+	"-source-path+=$ASSUKAR_IN_PATH/view/src"
+	"-source-path+=$PRAIA_IN_PATH/domain/src"		
+	"-source-path+=$PRAIA_IN_PATH/assets/src"
+	"-source-path+=$PRAIA_IN_PATH/tulum/view/src"
+	"-source-path+=$STARLING_PATH/starling/src"
+) 
+
+libraryPaths=(
+	"-library-path+=$SDK/frameworks/libs/player/$PLAYER_GLOBAL/playerglobal.swc"
+	"-library-path+=$SDK/frameworks/locale/en_US"
+	"-library-path+=$SDK/frameworks/libs/core.swc"
+	"-library-path+=$ASSUKAR_IN_PATH/libs/as3-signals.swc"
+	"-library-path+=$ASSUKAR_IN_PATH/libs/MinimalComps.swc"
+)
+
+defines=()
+
+
+. process.sh
---
+++ sh/praia/PRAIA-view-tulum.sh
@@ -0,0 +1,25 @@
+
+# PRAIA-view-tulum
+
+
+NAME="[PRAIA]-view-tulum"
+CLASSES=(
+	1 "$PRAIA_PATH\tulum\view"
+	0 "$PRAIA_PATH\assets"
+	0 "$PRAIA_PATH\domain"
+
+	0 "$ASSUKAR_PATH\view"
+	0 "$ASSUKAR_PATH\domain"
+ 	0 "$ASSUKAR_PATH\engine"
+ 	0 "$ASSUKAR_PATH\airong" 	
+
+ 	0 "$STARLING_PATH\starling"
+
+ 	0 "$ASSUKAR_PATH\extras"
+)
+LIBS=(
+	"$LIB_PATH\as3-signals.swc"
+	"$LIB_PATH\MinimalComps.swc" 
+)
+
+. sh/process.sh
