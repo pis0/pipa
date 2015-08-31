ada5a608ca273c40772be66076ed3652d3a3185a
Hsantos <igor.hos@gmail.com>
2015-08-31 16:57:51 -0300

    dev

---
+++ sh/magic/MAGIC-classicvegas.sh
@@ -0,0 +1,24 @@
+
+# MAGIC-classicvegas
+
+
+NAME="[MAGIC]-classicvegas"
+CLASSES=(
+	1 "$MAGIC_PATH\classicvegas"
+	0 "$MAGIC_PATH\assets"
+	0 "$MAGIC_PATH\domain"
+	
+	0 "$ASSUKAR_PATH\domain"
+ 	0 "$ASSUKAR_PATH\engine"
+ 	0 "$ASSUKAR_PATH\airong"
+ 	0 "$ASSUKAR_PATH\extras"
+	0 "$ASSUKAR_PATH\view"
+
+ 	0 "$STARLING_PATH\starling"
+)
+LIBS=(
+	"$LIB_PATH\as3-signals.swc"
+	"$LIB_PATH\MinimalComps.swc"
+)
+
+. sh/process.sh
