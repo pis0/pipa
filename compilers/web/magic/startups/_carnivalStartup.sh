8203684375a5e2519de503d8603a97333cb5ea98
pis0 <msdraco@gmail.com>
2017-02-06 12:29:01 -0200

    dev

--- compilers/web/magic/startups/_carnivalStartup.sh
+++ compilers/web/magic/startups/_carnivalStartup.sh
@@ -13,13 +13,13 @@ sourcePaths=(
   "-source-path+=$ASSUKAR_IN_PATH/airong/src"
   "-source-path+=$ASSUKAR_IN_PATH/engine/src"
   "-source-path+=$ASSUKAR_IN_PATH/domain/src" 
-  "-source-path+=$ASSUKAR_IN_PATH/fb/src"
+  # "-source-path+=$ASSUKAR_IN_PATH/fb/src"
   "-source-path+=$ASSUKAR_IN_PATH/view/src"
   "-source-path+=$ASSUKAR_IN_PATH/extras/src" 
 
   "-source-path+=$STARLING_PATH/starling/src"
 
-  "-source-path+=$MAGIC_IN_PATH/fbsource/src" 
+  # "-source-path+=$MAGIC_IN_PATH/fbsource/src" 
   "-source-path+=$MAGIC_IN_PATH/assets/src"  
   "-source-path+=$MAGIC_IN_PATH/domain/src"   
 
