1a9cf3c874a48c1e2c6300f7d91e66b040b6d62c
Diogo Carlomagno <diogocarlomagno@gmail.com>
2017-10-11 10:48:06 -0300

    dev

--- compilers/_MAIN_WEB_20.sh
+++ compilers/_MAIN_WEB_20.sh
@@ -53,31 +53,34 @@ DEBUG=false
 # PRAIA 
 
 # # # # assets   
-# . ./web/praia/assets/_hud.sh 
-# . ./web/praia/assets/_lobby.sh
-# . ./web/praia/assets/_bela.sh 
+. ./web/praia/assets/_hud.sh 
+. ./web/praia/assets/_lobby.sh
+
+. ./web/praia/assets/_bela.sh 
+. ./web/praia/assets/_rosa.sh
+. ./web/praia/assets/_maragogi.sh 
+. ./web/praia/assets/_noronha.sh  
+. ./web/praia/assets/_kauai.sh  
+. ./web/praia/assets/_keys.sh  
+. ./web/praia/assets/_azores.sh    
+
 # . ./web/praia/assets/_ilhabela.sh
 # . ./web/praia/assets/_ipanema.sh
 # . ./web/praia/assets/_jurere.sh
 # . ./web/praia/assets/_paraty.sh
 # . ./web/praia/assets/_itacare.sh
-# . ./web/praia/assets/_rosa.sh
-# . ./web/praia/assets/_maragogi.sh 
 # . ./web/praia/assets/_pipa.sh 
-# . ./web/praia/assets/_noronha.sh  
 # . ./web/praia/assets/_jeri.sh  
-# . ./web/praia/assets/_tulum.sh   
-# . ./web/praia/assets/_kauai.sh  
+# . ./web/praia/assets/_tulum.sh
 # . ./web/praia/assets/_ubatuba.sh   
 # . ./web/praia/assets/_menton.sh   
 # . ./web/praia/assets/_formentera.sh   
 # . ./web/praia/assets/_cabos.sh    
 # . ./web/praia/assets/_malibu.sh      
 # . ./web/praia/assets/_tenerife.sh  
-# . ./web/praia/assets/_keys.sh  
 # . ./web/praia/assets/_buzios.sh
-# . ./web/praia/assets/_sanvito.sh    
-# . ./web/praia/assets/_azores.sh    
+# . ./web/praia/assets/_sanvito.sh   
+
 
 # loaders
 . ./web/praia/loaders/_canvasMainSwf.sh
